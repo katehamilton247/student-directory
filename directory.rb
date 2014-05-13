e36ce14f104de646008da06724b91407eb88e929
katehamilton247 <katehamilton247@gmail.com>
2014-05-13 17:01:45 +0100

    Adding an array

--- directory.rb
+++ directory.rb
@@ -1,24 +1,46 @@
 student_count = 19
-#first we pring the list of students
+
+students = [
+"Nadav Matalon",
+"Fitsum Teklehaimanot",
+"Roi Driscoll",
+"James Whyte",
+"Margherita Serino",
+"Vaidas Mykolaitis",
+"Johann Bornman",
+"Kate Hamilton",
+"James Keap",
+"Nic Yeeles",
+"Julie Walker",
+"Will Allen",
+"Julia Tan",
+"Federico Maffei",
+"Jamie Patel",
+"Faezrah Rizalman",
+"Josh Fail-Brown",
+"Sasha Cooper",
+"Nicolai DTH"
+]
+
 print "The students of my cohort at Makers Academy\n-------------------------------------------\n"
-puts "Nadav Matalon"
-puts "Fitsum Teklehaimanot"
-puts "Roi Driscoll"
-puts "James Whyte"
-puts "Margherita Serino"
-puts "Vaidas Mykolaitis"
-puts "Johann Bornman"
-puts "Kate Hamilton"
-puts "James Keap"
-puts "Nic Yeeles"
-puts "Julie Walker"
-puts "Will Allen"
-puts "Julia Tan"
-puts "Federico Maffei"
-puts "Jamie Patel"
-puts "Faezrah Rizalman"
-puts "Josh Fail-Brown"
-puts "Sasha Cooper"
-puts "Nicolai DTH"
-#finally we print the total
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+
 puts "Overall, we have #{student_count} great students."
